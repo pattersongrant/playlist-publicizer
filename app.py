45bae227b61c7b7405653fa8df217001aead55e8
Grant Patterson <pattersongrant06@gmail.com>
2024-04-24 12:28:14 -0400

    rotated secret

--- app.py
+++ app.py
@@ -3,10 +3,7 @@ from spotipy.oauth2 import SpotifyOAuth
 
 scope = "playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public"
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id="25ce207f670f461fad8166716fd46836", client_secret="9d5157fd4b774b04bd68886a1e5ff49e", redirect_uri="http://localhost/"))
-#export SPOTIPY_CLIENT_ID="25ce207f670f461fad8166716fd46836"
-#export SPOTIPY_CLIENT_SECRET="9d5157fd4b774b04bd68886a1e5ff49e"
-#export SPOTIPY_REDIRECT_URI="http://localhost/"
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id="25ce207f670f461fad8166716fd46836", client_secret="*****", redirect_uri="http://localhost/"))
 
 for x in range(10):
     results = sp.current_user_playlists(limit=50, offset=50*x)
